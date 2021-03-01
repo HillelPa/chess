a8e0dcc61a33498a718d1d2f1210070215b9a262
HillelPa <64850372+HillelPa@users.noreply.github.com>
2021-03-01 18:17:04 +0100

    Update Echiquier.java
    C'est pa bo on trouvera mieux plus tard

--- Class/Echiquier.java
+++ Class/Echiquier.java
@@ -6,14 +6,39 @@ public class Echiquier {
 		for(int i = 0; i<8; i++){
 			for(int j = 0; j<8; j++){
 				if(i % 2 == 1 ^ j % 2 == 1){
-				echiquier[i][j] = new Case(false, new Pion(true, i));
-				System.out.println("noire");
+				echiquier[i][j] = new Case(false);
 				}else{
-				echiquier[i][j] = new Case(true, new Pion(true, i));
-				System.out.println("blanche");
+				echiquier[i][j] = new Case(true);
 				}
 			}
 		}
+		
+		echiquier[0][0].piece = new Tour(false);
+		echiquier[0][1].piece = new Cavalier(false);
+		echiquier[0][2].piece = new Fou(false);
+		echiquier[0][3].piece = new Roi(false);
+		echiquier[0][4].piece = new Reine(false);
+		echiquier[0][5].piece = new Fou(false);
+		echiquier[0][6].piece = new Cavalier(false);
+		echiquier[0][7].piece = new Tour(false);
+		
+		for(int j = 0; j<8; j++){
+			echiquier[1][j].piece = new Pion(false);
+		}
+		
+		echiquier[7][0].piece = new Tour(true);
+		echiquier[7][1].piece = new Cavalier(true);
+		echiquier[7][2].piece = new Fou(true);
+		echiquier[7][3].piece = new Roi(true);
+		echiquier[7][4].piece = new Reine(true);
+		echiquier[7][5].piece = new Fou(true);
+		echiquier[7][6].piece = new Cavalier(true);
+		echiquier[7][7].piece = new Tour(true);
+		
+		for(int j = 0; j<8; j++){
+			echiquier[6][j].piece = new Pion(true);
+		}
+		
 	}
 	
 	public void afficher(){
