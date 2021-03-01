f48501ecf1c6e293507979d9c55d7829955ed6fa
HillelPa <64850372+HillelPa@users.noreply.github.com>
2021-03-01 19:50:22 +0100

    Update Pion.java

--- Class/Pion.java
+++ Class/Pion.java
@@ -1,9 +1,19 @@
+import java.awt.*;
+	
 public class Pion extends Piece {
 
+
+	
 	public Pion(boolean aCouleur){ // c true : blanc ; c false : noir
 	
 	super("Pion",0,aCouleur);
-	valeur = 0;
+	
+	Toolkit T = Toolkit.getDefaultToolkit();
+	if(aCouleur){
+            image = T.getImage("Pion_Blanc.png");
+        }else{
+            image = T.getImage("Pion_Noir.png");
+        }
 	}
 	
 	public String toString(){
