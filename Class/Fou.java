8557a7f7f38d6a9b7bc5fe0d512c25a635c1390a
HillelPa <64850372+HillelPa@users.noreply.github.com>
2021-03-01 19:51:07 +0100

    Update Fou.java

--- Class/Fou.java
+++ Class/Fou.java
@@ -1,8 +1,18 @@
+import java.awt.*;
+
 public class Fou extends Piece {
 
     public Fou(boolean aCouleur){ // c true : blanc ; c false : noir
 
         super("Fou",4,aCouleur);
+        
+        Toolkit T = Toolkit.getDefaultToolkit();
+
+        if(aCouleur){
+            image = T.getImage("Fou_Blanc.png");
+        }else{
+            image = T.getImage("Fou_Noir.png");
+        }
     }
 
     public String toString(){
