33017660d22157c2d1851f1c3fbe4b51eb85935d
HillelPa <64850372+HillelPa@users.noreply.github.com>
2021-03-01 19:50:48 +0100

    Update Reine.java

--- Class/Reine.java
+++ Class/Reine.java
@@ -1,7 +1,17 @@
-public class Reine extends Piece {
+import java.awt.*;
 
+public class Reine extends Piece {
+	
+	
     public Reine(boolean aCouleur){
         super("Reine",5,aCouleur);
+        
+        Toolkit T = Toolkit.getDefaultToolkit();
+        if(aCouleur){
+            image = T.getImage("Reine_Blanche.png");
+        }else{
+            image = T.getImage("Reine_Noire.png");
+        }
     }
 
     public String toString(){
