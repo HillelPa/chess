91d3b679bbedbd3ab3a7fe386288ba0e51daf126
HillelPa <64850372+HillelPa@users.noreply.github.com>
2021-03-01 19:51:24 +0100

    Update Roi.java

--- Class/Roi.java
+++ Class/Roi.java
@@ -1,8 +1,18 @@
+import java.awt.*;
+
 public class Roi extends Piece {
 
+	
     public Roi(boolean aCouleur){ // c true : blanc ; c false : noir
 
         super("Roi",6,aCouleur);
+        
+        Toolkit T = Toolkit.getDefaultToolkit();
+        if(aCouleur){
+            image = T.getImage("Roi_Blanc.png");
+        }else{
+            image = T.getImage("Roi_Noir.png");
+        }
     }
 
     public String toString(){
