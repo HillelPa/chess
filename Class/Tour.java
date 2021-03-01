93f3158a1b5fa750535b74eaf1f65bf363ea5634
HillelPa <64850372+HillelPa@users.noreply.github.com>
2021-03-01 19:51:44 +0100

    Update Tour.java

--- Class/Tour.java
+++ Class/Tour.java
@@ -1,8 +1,17 @@
+import java.awt.*;
+
 public class Tour extends Piece {
-
-    public Tour(boolean aCouleur){ // c true : blanc ; c false : noir
-
+	
+    public Tour(boolean aCouleur){ 		// c true : blanc ; c false : noir
+        
         super("Tour",3,aCouleur);
+        Toolkit T = Toolkit.getDefaultToolkit();
+        
+        if(aCouleur){
+            image = T.getImage("Tour_Blanche.png");
+        }else{
+            image = T.getImage("Tour_Noire.png");
+        }
     }
 
     public String toString(){
