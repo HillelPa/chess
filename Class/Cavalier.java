19b71a9a5c9749c1cffad634a3b03e22ab2f4e6f
HillelPa <64850372+HillelPa@users.noreply.github.com>
2021-03-01 19:50:08 +0100

    Update Cavalier.java

--- Class/Cavalier.java
+++ Class/Cavalier.java
@@ -1,8 +1,18 @@
-public class Cavalier extends Piece {
+import java.awt.*;
 
+public class Cavalier extends Piece {
+	
+	
     public Cavalier(boolean aCouleur){ // c true : blanc ; c false : noir
 
-        super("Cavalier",2,aCouleur);
+        super("Cavalier",2,aCouleur); 
+          
+        Toolkit T = Toolkit.getDefaultToolkit();
+        if(aCouleur){
+            image = T.getImage("Cavalier_Blanc.png");
+        }else{
+            image = T.getImage("Cavalier_Noir.png");
+        }
     }
 
     public String toString(){
