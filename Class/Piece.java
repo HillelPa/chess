8d8bee4dad8a30a7d5a058443952daf23ec6fbdd
maxoulazone13 <65181493+maxoulazone13@users.noreply.github.com>
2021-03-01 09:37:24 +0100

    Class Piece

---
+++ Class/Piece.java
@@ -0,0 +1,27 @@
+public abstract class Piece {
+	
+	//Nom de la piece
+	private String nom;
+	
+	//Valeur de la piece
+	private int valeur;
+	
+	//Couleur de la piece, false noir, true blanc
+	private boolean couleur;
+	
+	// Coordonnées
+	private int coordonneeX;
+	private int coordonneeY;
+	
+	//contructeur de la piece (nom, couleur, coordonnées)
+	public Piece(String aNom, boolean aCouleur, int aCoordonneeX, int aCoordonneeY) {
+		
+		nom = aNom;
+		couleur = aCouleur;
+		CoordonneeX = aCoordonneeX;
+		CoordonneeY = aCoordonneeY;
+		
+	}
+	
+	//rajouter eventuellement ici une méthode test de déplacement unique à chaque type de piece
+}
