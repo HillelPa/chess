2a03086e74c8fd6bd93a066d8469b7ba8d30b0cf
HillelPa <64850372+HillelPa@users.noreply.github.com>
2021-03-01 19:49:28 +0100

    Update Case.java

--- Class/Case.java
+++ Class/Case.java
@@ -3,8 +3,16 @@ public class Case {
 	public boolean couleur; // true : blanche ; false : noire
 	public Piece piece;
 	
+	public Case(boolean aCouleur){ //case libre
+		couleur = aCouleur;
+	}
+	
 	public Case(boolean aCouleur, Piece aPiece){
 		couleur = aCouleur;
 		piece = aPiece;
+	}
+	
+	public String toString(){
+		return "case "+couleur+" avec comme piece "+piece;
 	}
 }
