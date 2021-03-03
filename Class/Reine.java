c378b98e0767727711f71603f99f011a743fa9ba
HillelPa <64850372+HillelPa@users.noreply.github.com>
2021-03-03 12:29:12 +0100

    Update Reine.java

--- Class/Reine.java
+++ Class/Reine.java
@@ -1,16 +1,16 @@
 import java.awt.*;
+import javax.swing.*;
 
 public class Reine extends Piece {
 	
 	
     public Reine(boolean aCouleur){
         super("Reine",5,aCouleur);
-        
-        Toolkit T = Toolkit.getDefaultToolkit();
+
         if(aCouleur){
-            image = T.getImage("Reine_Blanche.png");
+            image = new JLabel(new ImageIcon("Reine_Blanche.png"));
         }else{
-            image = T.getImage("Reine_Noire.png");
+            image = new JLabel(new ImageIcon("Reine_Noire.png"));
         }
     }
 
@@ -21,16 +21,16 @@ public class Reine extends Piece {
             return "qB";
         }
     }
-    
+
     public boolean typeDeplacement(Deplacement deplacement){
-		
-		// La reine peut se deplacer de sur les lignes et en diagonale
-		
-		if(Math.abs(deplacement.getDeplacementX()) - Math.abs(deplacement.getDeplacementY()) == 0 | 
-		   deplacement.getDeplacementX() * deplacement.getDeplacementY() == 0){
-			
-			return true;
-		}
-		return false;
-	}
+
+        // La reine peut se deplacer de sur les lignes et en diagonale
+
+        if(Math.abs(deplacement.getDeplacementX()) - Math.abs(deplacement.getDeplacementY()) == 0 |
+                deplacement.getDeplacementX() * deplacement.getDeplacementY() == 0){
+
+            return true;
+        }
+        return false;
+    }
 }
