696fb7abac41b71864d2abb6c659909d11270c70
HillelPa <64850372+HillelPa@users.noreply.github.com>
2021-03-03 12:29:37 +0100

    Update Pion.java

--- Class/Pion.java
+++ Class/Pion.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import javax.swing.*;
 	
 public class Pion extends Piece {
 
@@ -7,12 +8,11 @@ public class Pion extends Piece {
 	public Pion(boolean aCouleur){ // c true : blanc ; c false : noir
 	
 	super("Pion",0,aCouleur);
-	
-	Toolkit T = Toolkit.getDefaultToolkit();
+
 	if(aCouleur){
-            image = T.getImage("Pion_Blanc.png");
+            image = new JLabel(new ImageIcon("Pion_Blanc.png"));
         }else{
-            image = T.getImage("Pion_Noir.png");
+            image = new JLabel(new ImageIcon("Pion_Noir.png"));
         }
 	}
 	
@@ -23,12 +23,12 @@ public class Pion extends Piece {
 			return "pB";
 		}
 	}
-	
+
 	public boolean typeDeplacement(Deplacement deplacement){
-		
+
 		//Le pion ne peut pas se deplacer sur X et peut se deplacer de 1 ou 2 case en avant selon se position de départ
 		//Ici n'est pas codé le fait de pouvoir manger une pièce en diagonale
-		
+
 		if (deplacement.getDeplacementX() == 0){
 			if(Math.abs(deplacement.getDeplacementY()) == 2 && deplacement.getDepart().getY() == 6){
 				return true;
