ef04a167c4a0b1edad9980b3922976d8f7fb0466
HillelPa <64850372+HillelPa@users.noreply.github.com>
2021-03-03 12:25:11 +0100

    Update Cavalier.java

--- Class/Cavalier.java
+++ Class/Cavalier.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import javax.swing.*;
 
 public class Cavalier extends Piece {
 	
@@ -6,31 +7,31 @@ public class Cavalier extends Piece {
     public Cavalier(boolean aCouleur){ // c true : blanc ; c false : noir
 
         super("Cavalier",2,aCouleur); 
-          
-        Toolkit T = Toolkit.getDefaultToolkit();
+
         if(aCouleur){
-            image = T.getImage("Cavalier_Blanc.png");
+            image = new JLabel(new ImageIcon("Cavalier_Blanc.png"));
         }else{
-            image = T.getImage("Cavalier_Noir.png");
+            image = new JLabel(new ImageIcon("Cavalier_Noir.png"));
         }
     }
 
     public String toString(){
         if(couleur){
-            return "cB";
+            return "cW";
         }else{
-            return "cN";
+            return "cB";
         }
     }
-    
+
     public boolean typeDeplacement(Deplacement deplacement){
-		
-		//Le cavalier se déplace en forme de L
-		
-		if((Math.abs(deplacement.getDeplacementX()) == 2 && Math.abs(deplacement.getDeplacementY()) == 1 )| 
-		   (Math.abs(deplacement.getDeplacementX()) == 1 && Math.abs(deplacement.getDeplacementY()) == 2)){
-			   return true;
-		}
-		return false;
-	}
+
+        //Le cavalier se déplace en forme de L
+
+        if((Math.abs(deplacement.getDeplacementX()) == 2 && Math.abs(deplacement.getDeplacementY()) == 1 )|
+                (Math.abs(deplacement.getDeplacementX()) == 1 && Math.abs(deplacement.getDeplacementY()) == 2)){
+            return true;
+        }
+        return false;
+    }
+
 }
