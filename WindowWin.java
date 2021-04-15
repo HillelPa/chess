95c614a51697c5b935f5e1b5e2ea0f8818b840be
spluf17 <59790575+spluf17@users.noreply.github.com>
2021-04-15 15:34:30 +0200

    Add files via upload

--- WindowWin.java
+++ WindowWin.java
@@ -1,23 +1,55 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class WindowWin extends JFrame implements ActionListener{
 	
+	JButton leave;
+	JButton restart;
+	JPanel back;
+	WindowWin endGame;
+	
     public WindowWin(){
+    	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    	int width = (int) screenSize.getWidth();
+    	int height = (int) screenSize.getHeight();
 
+    	
         new JFrame("FIN DE LA PARTIE");
-        setSize(400,200);
+        setSize((int)(width /3.0),(int)(height/3.0));
+                
+        back = new JPanel();
+        back.setBounds(-100,-100,this.getWidth()+100,this.getHeight()+100);
+        back.setBackground(Color.DARK_GRAY);
+        back.setLayout(null);
+        back.setVisible(true);
+        
+        
+        leave = new JButton("Quitter");
+        leave.setBounds(2*(int)(this.getWidth()/3.0),2*(int)(this.getHeight()/3.0),(int)(this.getWidth()/4.0),(int)(this.getHeight()/6.0));
+        leave.setForeground(Color.DARK_GRAY);
+        leave.setBackground(Color.GREEN);
+        leave.addActionListener(this);
+        back.add(leave);
+        
+     
+        restart = new JButton("Recommencer");
+        restart.setBounds((int)(this.getWidth()/12.0),2*(int)(this.getHeight()/3.0),(int)(this.getWidth()/4.0),(int)(this.getHeight()/6.0));
+        restart.setForeground(Color.DARK_GRAY);
+        restart.setBackground(Color.GREEN);
+        restart.addActionListener(this);
+        back.add(restart);
+        
+        add(back);
         setLocationRelativeTo(null);
         setResizable(false);
-        setVisible(true);
+        
         setAlwaysOnTop(true);
 
     }
     
     public WindowWin(boolean couleur){
-    	WindowWin endGame = new WindowWin();
+    	
         String s = "Echec et mat, les ";
             if(couleur){
                 s += "Blancs ";
@@ -25,29 +57,45 @@ public class WindowWin extends JFrame implements ActionListener{
                 s += "Noirs ";
             }
         s += "ont gagné !";
-
-        JLabel mess = new JLabel(s, JLabel.CENTER);
-
-        endGame.add(mess);
+        
+        System.out.println(s);
+        JLabel mess = new JLabel(s);
+        mess.setBounds(0,0,this.getWidth(),(int)(this.getHeight()/2.0));
+        
+        
+        endGame = new WindowWin(); 
+        endGame.setVisible(true);
+        endGame.add(back);
+        back.add(mess);                
     }
     
     public WindowWin(boolean couleur,boolean tps){
-    	WindowWin endGame = new WindowWin();
+
         String s = "Le temps limite des ";
             if(!couleur){
-                s += "Blancs est dépassé, les Noirs";
+                s += "Blancs est dépassé, les Noirs ";
             }else{
                 s += "Noirs est dépassé, les Blancs ";
             }
         s += "ont gagné !";
-
-        JLabel mess = new JLabel(s, JLabel.CENTER);
-
-        endGame.add(mess);
+        
+        System.out.println(s);
+    	endGame = new WindowWin();
+    	endGame.setVisible(true);
+        JLabel mess = new JLabel(s);
+        mess.setBounds(0,0,this.getWidth(),(int)(this.getHeight()/2.0));
+        mess.setVisible(true);
+                
     }
 
-	/*public void actionPerformed(ActionEvent e) {
-		WindowWin endGame = new WindowWin();
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == leave){
+			System.exit(0);
+		}
+        if(e.getSource() == restart){
+        	new Home();
+        }
+			
+	}
 		
-	}*/
 }
